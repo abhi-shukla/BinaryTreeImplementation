02a75086a990c6137b05e56b1aae72fb4eb6c650
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-06-08 20:44:03 -0700

    Initial console app
     - Add BTNode class

---
+++ BinaryTreeImplementation/BinaryTreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinaryTreeImplementation
+{
+    public class BTNode<TNode> : IComparable<TNode>
+        where TNode : IComparable<TNode>
+    {
+        public BTNode(TNode value)
+        {
+            Value = value;
+        }
+
+        public BTNode<TNode> Left { get; set; }
+        public BTNode<TNode> Right { get; set; }
+
+        public TNode Value { get; set; }
+
+        public int CompareTo(TNode other)
+        {
+            return Value.CompareTo(other);
+        }
+    }
+}
