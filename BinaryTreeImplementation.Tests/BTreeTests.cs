ca0211c42deb27d64c7b92c9511fff714afe33dc
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-06-09 00:35:21 -0700

    Add more functionality and tests

---
+++ BinaryTreeImplementation.Tests/BTreeTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinaryTreeImplementation;
+
+namespace BinaryTreeImplementation.Tests
+{
+    [TestClass]
+    public class BTreeTests
+    {
+        [TestMethod]
+        public void AddElementToEmptyTree_ShouldAddElementAsHead()
+        {
+            // Arrange
+            var bTree = new BTree<int>();
+
+            // Act
+            bTree.Add(3);
+
+            // Assert
+            Assert.AreEqual(3, bTree._head.Value);
+        }
+
+        [TestMethod]
+        public void AddElement_SmallerValueThanHead_ShouldAddElementToLeft()
+        {
+            // Arrange
+            var bTree = new BTree<int>();
+            bTree.Add(3);
+
+            // Act
+            bTree.Add(2);
+
+            // Assert
+            Assert.AreEqual(2, bTree._head.Left.Value);
+        }
+
+        [TestMethod]
+        public void AddElement_LargerValueThanHead_ShouldAddElementToRight()
+        {
+            // Arrange
+            var bTree = new BTree<int>();
+            bTree.Add(3);
+
+            // Act
+            bTree.Add(4);
+
+            // Assert
+            Assert.AreEqual(4, bTree._head.Right.Value);
+        }
+    }
+}
---
+++ BinaryTreeImplementation/BTree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinaryTreeImplementation
+{
+    public class BTree<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        public BTNode<T> _head;
+        public int Count;
+
+        public void Add(T value)
+        {
+            if(_head == null)
+            {
+                _head = new BTNode<T>(value);
+            }
+            else
+            {
+                AddTo(_head, value);
+            }
+            Count++;
+        }
+
+        private void AddTo(BTNode<T> node, T value)
+        {
+            if(value.CompareTo(node.Value) < 0)
+            {
+                if(node.Left == null)
+                {
+                    node.Left = new BTNode<T>(value);
+                }
+                else
+                {
+                    AddTo(node, value);
+                }
+            }
+            else
+            {
+                if(node.Right == null)
+                {
+                    node.Right = new BTNode<T>(value);
+                }
+                else
+                {
+                    AddTo(node.Right, value);
+                }
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            BTNode<T> parent = null;
+            return FindWithParent(value, out parent) != null;
+        }
+
+        private object FindWithParent(T value, out BTNode<T> parent)
+        {
+            BTNode<T> current = _head;
+            parent = null;
+
+            while(current != null)
+            {
+                var compareResult = current.CompareTo(value);
+
+                if(compareResult > 0)
+                {
+                    parent = current;
+                    current = current.Left;
+                }
+                else if(compareResult < 0)
+                {
+                    parent = current;
+                    current = current.Right;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return current;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
